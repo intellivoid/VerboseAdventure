027d2e409e98dbec08712ba693189c12a90007e7
Netkas <netkas@intellivoid.info>
2021-01-18 11:32:18 -0500

    Updated for PPM 1.1.2.0

--- src/VerboseAdventure/Exceptions/FatalErrorException.php
+++ src/VerboseAdventure/Exceptions/FatalErrorException.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace VerboseAdventure\Exceptions;
 
+use ErrorException;
+
 /**
  * This exception is thrown when a fatal error occurs.
  *
  * @author Stefano Arlandini <sarlandini@alice.it>
  */
-final class FatalErrorException extends \ErrorException
+final class FatalErrorException extends ErrorException
 {
 }
--- src/VerboseAdventure/Exceptions/SilencedErrorException.php
+++ src/VerboseAdventure/Exceptions/SilencedErrorException.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace VerboseAdventure\Exceptions;
 
+use ErrorException;
+
 /**
  * Error that has been captured by the {@see ErrorHandler}, but that was silenced
  * with `@` in the source code.
  */
-final class SilencedErrorException extends \ErrorException
+final class SilencedErrorException extends ErrorException
 {
 }
